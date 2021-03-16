15c3322e32f946f155b506bea8db671aa1f6653f
JY <kims2606@gmail.com>
2021-03-17 08:20:31 +0900

    Springboot 메서드 레벨 보안. @PostAuthorize

--- resourceServer-springboot/src/main/java/com/jy/study/oauth2/resourceServer/controller/UsersController.java
+++ resourceServer-springboot/src/main/java/com/jy/study/oauth2/resourceServer/controller/UsersController.java
@@ -1,6 +1,8 @@
 package com.jy.study.oauth2.resourceServer.controller;
 
+import com.jy.study.oauth2.resourceServer.model.User;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -35,6 +37,16 @@ public class UsersController {
             @AuthenticationPrincipal Jwt jwt
     ) {
         return "updated user name as " + name + " and JWT subject " + jwt.getSubject();
+    }
+
+    /*
+     * 메서드를 실행하고 권한을 체크한다.
+     * returnObject(return 하는 User 객체)의 id와 argument인 jwt의 subject가 같은지 확인.
+     */
+    @PostAuthorize("returnObject.id == #jwt.subject")
+    @GetMapping("{id}")
+    public User getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return new User(id, "joonyeop", "kim");
     }
 
 }
