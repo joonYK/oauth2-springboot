3446d51086d277dbbee9516f4cd2740c27ac8283
JY <kims2606@gmail.com>
2021-03-17 08:04:15 +0900

    Springboot 메서드 레벨 보안. @Secured, @PreAuthorize

--- resourceServer-springboot/src/main/java/com/jy/study/oauth2/resourceServer/controller/UsersController.java
+++ resourceServer-springboot/src/main/java/com/jy/study/oauth2/resourceServer/controller/UsersController.java
@@ -1,8 +1,10 @@
 package com.jy.study.oauth2.resourceServer.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -12,4 +14,27 @@ public class UsersController {
     public String status() {
         return "Working..";
     }
+
+    //developer Role을 가진 User만 해당 api를 실행 가능.
+    @Secured("ROLE_developer")
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable String id) {
+        return "Deleted user with id " + id;
+    }
+
+    /*
+     * 메서드를 실행하기 전에 권한을 체크한다.
+     * developer Role이 있는지 체크하거나 (hasAuthority('ROLE_developer')로 체크해도 된다.) ,
+     * 메서드의 arguments를 이용해서 id가 jwt의 getSubject 메서드를 사용해서 가져온 값과 일치하는지 확인한다.
+     */
+    @PreAuthorize("hasRole('developer') or #id == #jwt.subject")
+    @PutMapping("/{id}/name")
+    public String updateUserName(
+            @PathVariable String id,
+            String name,
+            @AuthenticationPrincipal Jwt jwt
+    ) {
+        return "updated user name as " + name + " and JWT subject " + jwt.getSubject();
+    }
+
 }
