0cd14230f84da7eb78f66678232bf81be5ab42b2
JY <kims2606@gmail.com>
2021-03-19 09:09:50 +0900

    ApiGateway 모듈 설정 수정

---
+++ springProject/apiGateway/src/main/java/com/jy/study/oauth2/apiGateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jy.study.oauth2.apiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
