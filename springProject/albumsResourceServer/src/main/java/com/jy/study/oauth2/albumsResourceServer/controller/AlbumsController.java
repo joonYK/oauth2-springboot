fe1108d64c80847ce82469211ce136cd9bc2af55
JY <kims2606@gmail.com>
2021-03-20 23:13:12 +0900

    albums 리소스 서버 생성

---
+++ springProject/albumsResourceServer/src/main/java/com/jy/study/oauth2/albumsResourceServer/controller/AlbumsController.java
@@ -0,0 +1,35 @@
+package com.jy.study.oauth2.albumsResourceServer.controller;
+
+import com.jy.study.oauth2.albumsResourceServer.model.Album;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/albums")
+public class AlbumsController {
+    
+    @GetMapping
+    public List<Album> getAlbums() {
+        Album album1 = Album.builder()
+                .id("albumId1")
+                .userId("1")
+                .albumTitle("Album 1 title")
+                .albumDescription("Album 1 description")
+                .albumUrl("Album 1 URL")
+                .build();
+
+        Album album2 = Album.builder()
+                .id("albumId2")
+                .userId("2")
+                .albumTitle("Album 2 title")
+                .albumDescription("Album 2 description")
+                .albumUrl("Album 2 URL")
+                .build();
+
+        return Arrays.asList(album1, album2);
+    }
+}
---
+++ springProject/albumsResourceServer/src/main/java/com/jy/study/oauth2/albumsResourceServer/model/Album.java
@@ -0,0 +1,21 @@
+package com.jy.study.oauth2.albumsResourceServer.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class Album {
+
+    private String id;
+
+    private String userId;
+
+    private String albumTitle;
+
+    private String albumDescription;
+
+    private String albumUrl;
+}
