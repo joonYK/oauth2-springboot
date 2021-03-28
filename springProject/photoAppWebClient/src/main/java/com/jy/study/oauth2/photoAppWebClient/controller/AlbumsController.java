7109894875715cc304c92cc3ed22b3bbbccacf9b
JY <kims2606@gmail.com>
2021-03-28 21:17:39 +0900

    SpringProject photoAppWebClient 모듈 Oauth2 Client 적용

--- springProject/photoAppWebClient/src/main/java/com/jy/study/oauth2/photoAppWebClient/controller/AlbumsController.java
+++ springProject/photoAppWebClient/src/main/java/com/jy/study/oauth2/photoAppWebClient/controller/AlbumsController.java
@@ -1,18 +1,26 @@
 package com.jy.study.oauth2.photoAppWebClient.controller;
 
 import com.jy.study.oauth2.photoAppWebClient.model.Album;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class AlbumsController {
 
     @GetMapping("/albums")
-    public String getAlbums(Model model) {
+    public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
+
+        System.out.println("Principal = " + principal);
+
+        OidcIdToken idToken = principal.getIdToken();
+        String idTokenValue = idToken.getTokenValue();
+        System.out.println("idTokenValue = " + idTokenValue);
 
         Album album1 = Album.builder()
                 .id("albumOne")
