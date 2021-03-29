aa53ddb09610996ba883b9ae31f41d723428a9ae
JY <kims2606@gmail.com>
2021-03-29 12:35:56 +0900

    SpringBoot photoAppWebClient 모듈 AlbumsController JwtAccessToken 값 확인

--- springProject/photoAppWebClient/src/main/java/com/jy/study/oauth2/photoAppWebClient/controller/AlbumsController.java
+++ springProject/photoAppWebClient/src/main/java/com/jy/study/oauth2/photoAppWebClient/controller/AlbumsController.java
@@ -1,7 +1,13 @@
 package com.jy.study.oauth2.photoAppWebClient.controller;
 
 import com.jy.study.oauth2.photoAppWebClient.model.Album;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -13,8 +19,20 @@ import java.util.Arrays;
 @Controller
 public class AlbumsController {
 
+    @Autowired
+    private OAuth2AuthorizedClientService oAuth2ClientService;
+
     @GetMapping("/albums")
     public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+
+        OAuth2AuthorizedClient oauth2Client = oAuth2ClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(),
+                oauthToken.getName());
+
+        String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
+        System.out.println("jwtAccessToken = " + jwtAccessToken);
 
         System.out.println("Principal = " + principal);
 
