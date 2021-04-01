822839f7ecce8f08a647d96fb642d485d002c1e5
JY <kims2606@gmail.com>
2021-04-01 21:27:40 +0900

    SpringProject socialLoginWEbClient 모듈 생성

---
+++ springProject/socialLoginWebClient/src/main/java/com/jy/study/oauth2/socialLoginWebClient/SocialLoginWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.jy.study.oauth2.socialLoginWebClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLoginWebClientApplication.class, args);
+	}
+
+}
