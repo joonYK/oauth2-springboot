d6e4b727c81e9ce20ac292c1833a27e3a05bc5c6
JY <kims2606@gmail.com>
2021-04-02 08:09:11 +0900

    SpringProject socialLoginWebClient page 추가 (index, home)

---
+++ springProject/socialLoginWebClient/src/main/java/com/jy/study/oauth2/socialLoginWebClient/controller/HomePageController.java
@@ -0,0 +1,21 @@
+package com.jy.study.oauth2.socialLoginWebClient.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("/home")
+    public String displayHomePage(Model model, @AuthenticationPrincipal OAuth2User principal) {
+        if (principal != null) {
+            Object name = principal.getAttribute("name");
+            model.addAttribute("name", name);
+        }
+
+        return "home";
+    }
+}
