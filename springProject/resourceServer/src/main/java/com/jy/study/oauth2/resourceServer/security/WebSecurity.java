1409e1d4d8a0c715e7808c554a0ec0418e1cb15b
JY <kims2606@gmail.com>
2021-04-09 08:00:22 +0900

    SpringProject > spa-example > index.html  API Gateway 가 아닌 ResourceServer로 직접 요청. (ResourceServer에 cors 설정)

--- springProject/resourceServer/src/main/java/com/jy/study/oauth2/resourceServer/security/WebSecurity.java
+++ springProject/resourceServer/src/main/java/com/jy/study/oauth2/resourceServer/security/WebSecurity.java
@@ -1,11 +1,17 @@
 package com.jy.study.oauth2.resourceServer.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 //Method Level 보안 활성화
 @EnableGlobalMethodSecurity(
@@ -26,7 +32,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
          * 모든 request 권한 검사.
          * oauth2 리소스 서버 및 jwt 토큰 사용
          */
-        http.authorizeRequests()
+        http
+                //cors 설정(아래의 CorsConfigurationSource Bean 으로 설정한다)
+                .cors()
+             .and()
+                .authorizeRequests()
                 // GET "/users/status/check" url 대해
                 .antMatchers(HttpMethod.GET, "/users/status/check")
                     //profile scope를 포함하는지 체크. authority 체크는 prefix를 붙여서 검사.
@@ -34,6 +44,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
                     //develpoer Role을 포함하는지 체크. role 체크는 prefix를 붙이지 않고 검사.
                     .hasRole("developer")
+
 
                     //여러개의 Role을 체크
                     //.hasAnyRole("developer", "user")
@@ -43,5 +54,19 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .jwt()
                 //keycloak roles -> GrantedAuthority 변환
                 .jwtAuthenticationConverter(jwtAuthenticationConverter);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        //모든 url에 대해 cors 설정 적용
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 }
