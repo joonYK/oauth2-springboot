6b3a36c8ab62cb17b8896b92e825095671882701
JY <shutingz@naver.com>
2021-04-09 22:00:33 +0900

    SpringProject MyRemoteUserStorageProvider 모듈 추가 (keyclock remote storage)

---
+++ springProject/MyRemoteUserStorageProvider/src/main/java/com/jy/study/oauth2/myRemoteUserStorageProvider/MyRemoteUserStorageProviderApplication.java
@@ -0,0 +1,14 @@
+package com.jy.study.oauth2.myRemoteUserStorageProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MyRemoteUserStorageProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRemoteUserStorageProviderApplication.class, args);
+	}
+
+}
